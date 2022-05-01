8534d7dcff85ae6a2f26c9f8d2703eee6f8c644f
YuvarajSingh-0 <yuvarajsingh170@gmail.com>
2022-05-01 17:26:27 +0530

    Desining

--- app.js
+++ app.js
@@ -21,13 +21,6 @@ const todoSchema = new mongoose.Schema({
 });
 
 const users = new mongoose.model("users", todoSchema);
-// const user = new users({ username: "nys", password: "nys", todos: [] });
-// const user1 = new users({
-//   username: "nys1",
-//   password: "nys1",
-//   todos: ["blah1", "blah2"],
-// });
-// user.save();
 
 app.set("view-engine", "ejs");
 
@@ -38,31 +31,33 @@ app.use(
 );
 app.use(express.static("./public"));
 
-// const data = [];
-app.get("/", (req, res) => {
-  // res.json({val:'LOL'})
+app.get("/", (req,res)=>{
+  res.redirect("/login")
+})
+
+app.get("/login", (req, res) => {
   res.render("../public/login.ejs", { msg: "" });
 });
 
+app.get("/deletetask",(req,res)=>{
+  res.redirect("/login");
+})
+
+app.get("/addtask",(req,res)=>{
+  res.redirect("/login");
+})
+
 app.post("/login", async (req, res) => {
   var userData = await users.find({ username: req.body.username });
-  console.log("in /login outside if cond",userData)
 
   if (userData.length==0) {
-    console.log("in /login if cond",userData)
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    // userData = [{
-    //   username: req.body.username,
-    //   password: hashedPassword,
-    //   todos: [],
-    // }];
     var user = new users({
       username: req.body.username,
       password: hashedPassword,
       todos: [],
     });
     user.save();
-    console.log("in /login if cond after saving to database",user);
     res.render("../public/todo.ejs", { values: [], name: user.username });
   } else if (
     (userData[0].username == req.body.username) &&
@@ -77,26 +72,31 @@ app.post("/login", async (req, res) => {
   }
 });
 
+
 app.post("/addtask", async (req, res) => {
-  // res.render('../public/todo',{values:data});
-  console.log("in /addtask req.body",req.body)
   newtask = req.body.task;
-  //   console.log(req.body);
   var userData = await users.find({ username: req.body.name });
-  console.log("in /addtask\n",userData)
-  // userData[0].todos.push(newtask);
   var newtodos=userData[0].todos
   newtodos.push(newtask)
-  // var newtodos=userData[0].todos.push(newtask);
-  // console.log(newtodos,newtask)
   await users.updateOne({username:req.body.name},{$set:{todos:newtodos}})
-  // var user = new users(userData[0]);
-  // user.save();
   res.render("../public/todo.ejs", {
     values: userData[0].todos,
     name: userData[0].username,
   });
 });
+
+
+
+
+app.post("/deletetask", async (req,res)=>{
+  let user=await users.findOne({username:req.body.name})
+  old_todos=user.todos;
+  var updated_todos=old_todos.filter( todo =>{ return todo!=req.body.value})
+  await users.updateOne({username:req.body.name}, {$set:{todos:updated_todos}})
+  res.render('../public/todo.ejs',{name:user.username,values:updated_todos})
+})
+
+
 let port = process.env.PORT;
 
 if (port == null || port == "") {
@@ -104,6 +104,5 @@ if (port == null || port == "") {
 }
 
 app.listen(port, async () => {
-  console.log(await users.find({username:"nys"}));
-  console.log("Server is runnning at port: ",port);
+  console.log("Server is runnning at : localhost:"+port+"/");
 });
